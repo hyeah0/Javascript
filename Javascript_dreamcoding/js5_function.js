d13149423314758f9d10a7602626de8978397c84
hye <hye0ah0@gmail.com>
2022-03-23 22:32:45 +0900

    function

--- Javascript_dreamcoding/js5_function.js
+++ Javascript_dreamcoding/js5_function.js
@@ -37,5 +37,71 @@ function printArray(...args){
     }
     console.log('--------');
     args.forEach((arg) => console.log(arg));
+    console.log('--------');
 }
 printArray('포도','사과','키위','오렌지');
+
+// function expresstion
+// a. function declaration can be called earlier than it is defiend (hoisted)
+// b. function expression is created when the excution reaches it
+
+const print = function () { //anonymous function
+    console.log('anonymous function');
+}
+print();
+const printAagin = print;
+printAagin();
+
+
+// callback function using function expression
+function randomQuiz(answer, printYes, printNo){
+    if(answer === 'love you'){
+        printYes();
+    }
+    else{
+        printNo();
+    }
+}
+
+const printYes = function(){
+    console.log('yes!');
+};
+
+//nameed function
+//함수안에서 함수 자신을 부를때,
+//피보나치수나, 반복되는 평균값을 구할때
+const printNo = function printN(){
+    console.log('no!');
+    // printN();
+}
+
+randomQuiz('love you', printYes, printNo);
+randomQuiz('i do not love you', printYes, printNo);
+
+//return value
+function sum(a,b){
+    return a+b;
+}
+const result = sum(2,4);
+console.log(`sum : ${result}`);
+
+// arrow function
+// =anonymous function
+const dontUseArrow = function(){
+    console.log('do not use arrow function!');
+}
+const useArrow = () => console.log('use arrow function!');
+dontUseArrow();
+useArrow();
+
+const add1 = (e,f) => e + f;
+const add2 = function(e,f){
+    return e + f;
+};
+const add3 = (e,f) => {
+    return e + f;
+};
+
+const addResult = add1(3,4);
+console.log(addResult);
+
