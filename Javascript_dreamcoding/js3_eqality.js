a65aa3d7965acda001fdf979318107647c6d2c29
hye <88965526+hyeah0@users.noreply.github.com>
2023-04-04 18:50:34 +0900

    Delete js3_eqality.js

--- Javascript_dreamcoding/js3_eqality.js
+++
@@ -1,8 +0,0 @@
-const numfive = 5;
-const strfive = '5';
-
-console.log(numfive == strfive);
-console.log(numfive != strfive);
-
-console.log(numfive === strfive);
-console.log(numfive !== strfive);
